5ee2dbc85acb66333619630217390db8e5d12051
Casey Webster <casey.webster@gmail.com>
2015-10-26 17:35:01 +0000

    API changes in base abstract class
    Start overhaul of API to streamline user interface.
    
    Added a cmake file to clean build directories

--- src/cm1_base.f90
+++ src/cm1_base.f90
@@ -42,39 +42,38 @@ module ingest_cm1_base
          character(len=64)  :: basename
          integer            :: dtype
          character          :: grid
-         integer :: nx, ny, nz, nt, nv, dt
+         integer :: nx, ny, nz, nt, nv, dt, time_open
          real, dimension(:), allocatable :: x, y, z
          integer, dimension(:), allocatable :: times
          type (variable), dimension(:), allocatable  :: vars
-         logical :: isopen = .false., ismult = .false.
+         logical :: is_dataset_open = .false., is_time_open = .false.
       contains
          ! Private procedures
-         procedure, pass(self) :: check_open
-         procedure, pass(self) :: check_mult
+         procedure, pass(self) :: check_dataset_open
+         procedure, pass(self) :: check_time_open
          procedure, pass(self) :: cm1log
-
+         procedure(open_dataset_time_i), deferred, public ,pass(self) :: open_dataset_time
+         procedure(close_dataset_time_i), deferred, public ,pass(self) :: close_dataset_time
+         procedure(read3D_at_time_i), deferred, public ,pass(self) :: read3D_at_time
+         procedure(read2D_at_time_i), deferred, public ,pass(self) :: read2D_at_time
+         
          ! Public getter functions for generic dataset attributes
-         procedure, public ,pass(self) :: cm1_nx
-         procedure, public ,pass(self) :: cm1_ny
-         procedure, public ,pass(self) :: cm1_nz
-         procedure, public ,pass(self) :: cm1_nt
-         procedure, public ,pass(self) :: cm1_nv
-         procedure, public ,pass(self) :: cm1_x
-         procedure, public ,pass(self) :: cm1_y
-         procedure, public ,pass(self) :: cm1_z
-         procedure, public ,pass(self) :: cm1_t
-         procedure, public ,pass(self) :: getVarByName
+         procedure, public ,pass(self) :: get_nx
+         procedure, public ,pass(self) :: get_ny
+         procedure, public ,pass(self) :: get_nz
+         procedure, public ,pass(self) :: get_nt
+         procedure, public ,pass(self) :: get_nv
+         procedure, public ,pass(self) :: get_x
+         procedure, public ,pass(self) :: get_y
+         procedure, public ,pass(self) :: get_z
+         procedure, public ,pass(self) :: get_t
+         procedure, public ,pass(self) :: get_var_byname
          procedure, public ,pass(self) :: read3D
          procedure, public ,pass(self) :: read3DSlice
          procedure, public ,pass(self) :: read2D
-
-         ! deferred procedures
          procedure(open_cm1_i), deferred, public ,pass(self) :: open_cm1
          procedure(close_cm1_i),deferred, public ,pass(self) :: close_cm1
-         procedure(readMultStart_i), deferred, public ,pass(self) :: readMultStart
-         procedure(readMultStop_i), deferred, public ,pass(self) :: readMultStop
-         procedure(read3DMult_i), deferred, public ,pass(self) :: read3DMult
-         procedure(read2DMult_i), deferred, public ,pass(self) :: read2DMult
+         
    end type cm1_base
 
    abstract interface
@@ -94,30 +93,30 @@ module ingest_cm1_base
          class(cm1_base) :: self
       end function close_cm1_i
 
-      integer function readMultStart_i(self,time)
+      integer function open_dataset_time_i(self,time)
          import cm1_base
          class(cm1_base)          :: self
          integer, intent(in) :: time
-      end function readMultStart_i
+      end function open_dataset_time_i
 
-      integer function readMultStop_i(self)
+      integer function close_dataset_time_i(self)
          import cm1_base
          class(cm1_base) :: self
-      end function readMultStop_i
+      end function close_dataset_time_i
 
-      integer function read2DMult_i(self, varname, Field2D)
+      integer function read2D_at_time_i(self, varname, Field2D)
          import cm1_base
          class(cm1_base) :: self
          character(len=*), intent(in) :: varname
          real, dimension(self%nx,self%ny) :: Field2D
-      end function read2DMult_i
+      end function read2D_at_time_i
 
-      integer function read3DMult_i(self, varname, Field3D)
+      integer function read3D_at_time_i(self, varname, Field3D)
          import cm1_base
          class(cm1_base) :: self
          character(len=*), intent(in) :: varname
          real, dimension(self%nx,self%ny,self%nz) :: Field3D
-      end function read3DMult_i
+      end function read3D_at_time_i
    end interface
 
    !for logging
@@ -127,34 +126,31 @@ module ingest_cm1_base
    integer, public, parameter :: LOG_MSG   = 1002
    integer, public, parameter :: LOG_DEBUG = 1001
 
-   !integer :: min_loglevel = LOG_INFO
-   integer :: min_loglevel = LOG_DEBUG
+   integer :: min_loglevel = LOG_INFO
 
 contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function getVarByName(self,varname)
+   integer function get_var_byname(self,varname)
       implicit none
       class(cm1_base)                   :: self
       character(len=*), intent(in) :: varname
       integer :: i
 
-      if (.not. self%check_open('getVarByName')) then
-         getVarByName = 0
-         return
-      end if
+      get_var_byname = 0
+      if (.not. self%check_dataset_open('get_var_byname')) return
 
       do i = 1, self%nv
          if (varname.eq.self%vars(i)%varname) then
-            getVarByName = i
+            get_var_byname = i
             return
          endif
       end do
 
-      getVarByName = 0
-
-   end function getVarByName
+      call self%cm1log(LOG_WARN, 'get_var_byname', 'Requested variable does not exist: '//varname)
+      
+   end function get_var_byname
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -166,27 +162,27 @@ contains
       real, dimension(self%nx,self%ny) :: Field2D
       integer :: status
 
-      if (.not. self%check_open('read2D')) then
+      if (.not. self%check_dataset_open('read2D')) then
          read2D = 0
          return
       end if
 
-      status = self%readMultStart(time)
+      status = self%open_dataset_time(time)
       if (status.eq.0) then
          call self%cm1log(LOG_ERROR, 'read2D', 'Data open failure, aborting: ')
          read2D = 0
          return
       endif
 
-      status = self%read2DMult(varname, Field2D)
+      status = self%read2D_at_time(varname, Field2D)
       if (status.eq.0) then
          call self%cm1log(LOG_ERROR, 'read2D', 'Data read failure, aborting: ')
          read2D = 0
-         status = self%readMultStop()
+         status = self%close_dataset_time()
          return
       endif
 
-      status = self%readMultStop()
+      status = self%close_dataset_time()
       if (status.eq.0) then
          call self%cm1log(LOG_ERROR, 'read2D', 'Data close failure, aborting: ')
          read2D = 0
@@ -207,27 +203,27 @@ contains
       real, dimension(self%nx,self%ny,self%nz) :: Field3D
       integer :: status
 
-      if (.not. self%check_open('read3D')) then
+      if (.not. self%check_dataset_open('read3D')) then
          read3D = 0
          return
       end if
 
-      status = self%readMultStart(time)
+      status = self%open_dataset_time(time)
       if (status.eq.0) then
          call self%cm1log(LOG_ERROR, 'read3D', 'Data open failure, aborting: ')
          read3D = 0
          return
       endif
 
-      status = self%read3DMult(varname, Field3D)
+      status = self%read3D_at_time(varname, Field3D)
       if (status.eq.0) then
          call self%cm1log(LOG_ERROR, 'read3D', 'Data read failure, aborting: ')
          read3D = 0
-         status = self%readMultStop()
+         status = self%close_dataset_time()
          return
       endif
 
-      status = self%readMultStop()
+      status = self%close_dataset_time()
       if (status.eq.0) then
          call self%cm1log(LOG_ERROR, 'read3D', 'Data close failure, aborting: ')
          read3D = 0
@@ -248,7 +244,7 @@ contains
       integer :: status
       real, dimension(self%nx,self%ny,self%nz) :: Field3DFull
       
-      if (.not. self%check_open('read3DSlice')) then
+      if (.not. self%check_dataset_open('read3DSlice')) then
          read3DSlice = 0
          return
       end if
@@ -274,240 +270,234 @@ contains
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_nx(self)
+   integer function get_nx(self)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
 
-      if (.not. self%check_open('cm1_nx')) then
-         cm1_nx = 0
-         return
-      end if
+      get_nx = 0
+      if (.not. self%check_dataset_open('get_nx')) return
 
-      cm1_nx = self%nx
-   end function cm1_nx
+      get_nx = self%nx
+   end function get_nx
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_ny(self)
+   integer function get_ny(self)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
 
-      if (.not. self%check_open('cm1_ny')) then
-         cm1_ny = 0
-         return
-      end if
+      get_ny = 0
+      if (.not. self%check_dataset_open('get_ny')) return
 
-      cm1_ny = self%ny
-   end function cm1_ny
+      get_ny = self%ny
+   end function get_ny
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_nz(self)
+   integer function get_nz(self)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
 
-      if (.not. self%check_open('cm1_nz')) then
-         cm1_nz = 0
-         return
-      end if
+      get_nz = 0
+      if (.not. self%check_dataset_open('get_nz')) return
 
-      cm1_nz = self%nz
-   end function cm1_nz
+      get_nz = self%nz
+   end function get_nz
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_nt(self)
+   integer function get_nt(self)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
 
-      if (.not. self%check_open('cm1_nt')) then
-         cm1_nt = 0
-         return
-      end if
+      get_nt = 0
+      if (.not. self%check_dataset_open('get_nt')) return
 
-      cm1_nt = self%nt
-   end function cm1_nt
+      get_nt = self%nt
+   end function get_nt
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_nv(self)
+   integer function get_nv(self)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
 
-      if (.not. self%check_open('cm1_nv')) then
-         cm1_nv = 0
-         return
-      end if
+      get_nv = 0
+      if (.not. self%check_dataset_open('get_nv')) return
 
-      cm1_nv = self%nv
-   end function cm1_nv
+      get_nv = self%nv
+   end function get_nv
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_dt(self)
+   integer function get_dt(self)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
 
-      if (.not. self%check_open('cm1_dt')) then
-         cm1_dt = 0
-         return
-      end if
+      get_dt = 0
+      if (.not. self%check_dataset_open('get_dt')) return
 
-      cm1_dt = self%dt
-   end function cm1_dt
+      get_dt = self%dt
+   end function get_dt
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_x(self, cm1x)
+   integer function get_x(self, cm1x)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
       real, dimension(self%nx) :: cm1x
 
-      if (.not. self%check_open('cm1_x')) then
-         cm1_x = 0
-         return
-      end if
+      get_x = 0
+      if (.not. self%check_dataset_open('get_x')) return
 
       cm1x(:) = self%x(:)
-      cm1_x = 1
-   end function cm1_x
+      get_x = 1
+   end function get_x
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_y(self, cm1y)
+   integer function get_y(self, cm1y)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
       real, dimension(self%ny) :: cm1y
 
-      if (.not. self%check_open('cm1_x')) then
-         cm1_y = 0
-         return
-      end if
+      get_y = 0
+      if (.not. self%check_dataset_open('get_y')) return
 
       cm1y(:) = self%y(:)
-      cm1_y = 1
-   end function cm1_y
+      get_y = 1
+   end function get_y
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_z(self, cm1z)
+   integer function get_z(self, cm1z)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
       real, dimension(self%nz) :: cm1z
 
-      if (.not. self%check_open('cm1_x')) then
-         cm1_z = 0
-         return
-      end if
+      get_z = 0
+      if (.not. self%check_dataset_open('get_z')) return
 
       cm1z(:) = self%z(:)
-      cm1_z = 1
-   end function cm1_z
+      get_z = 1
+   end function get_z
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   integer function cm1_t(self, cm1t)
+   integer function get_t(self, cm1t)
       implicit none
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
       real, dimension(self%nt) :: cm1t
 
-      if (.not. self%check_open('cm1_x')) then
-         cm1_t = 0
-         return
-      end if
+      get_t = 0 
+      if (.not. self%check_dataset_open('get_x')) return
 
       cm1t(:) = self%times(:)
-      cm1_t = 1
-   end function cm1_t
+      get_t = 1
+   end function get_t
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   logical function check_open(self, funcname)
+   integer function get_time_open(self)
+     implicit none
+     class(cm1_base), intent(in) :: self
+
+     get_time_open = -1
+     if (.not. self%check_dataset_open('get_time_open')) return
+     if (.not. self%check_time_open('get_time_open')) return
+
+     get_time_open = self%time_open
+   end function get_time_open
+   
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+   logical function check_dataset_open(self, funcname)
       use, intrinsic :: iso_fortran_env, only: error_unit
-      class(cm1_base),intent(in) :: self
+      class(cm1_base), intent(in) :: self
       character(len=*), intent(in) :: funcname
 
       ! Is the dataset open (has the ctl file been scanned)
-      if (.not. self%isopen) then
+      if (.not. self%is_dataset_open) then
          call self%cm1log(LOG_ERROR, funcname, 'No dataset open, aborting')
-         check_open = .false.
+         check_dataset_open = .false.
          return
       end if
-      check_open = .true.
+      check_dataset_open = .true.
 
-   end function check_open
+   end function check_dataset_open
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-   logical function check_mult(self, funcname)
+   logical function check_time_open(self, funcname)
       use, intrinsic :: iso_fortran_env, only: error_unit
       class(cm1_base),intent(in) :: self
       character(len=*), intent(in) :: funcname
 
       ! Is the dataset open for reading (is the dat file open)
-      if (.not. self%ismult) then
+      if (.not. self%is_time_open) then
          call cm1log(self, LOG_ERROR, funcname, 'No multiread started, aborting')
-         check_mult = .false.
+         check_time_open = .false.
          return
       end if
-      check_mult = .true.
+      check_time_open = .true.
 
-   end function check_mult
+   end function check_time_open
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
    subroutine cm1log(self, loglevel, funcname, message)
-      use, intrinsic :: iso_fortran_env, only: output_unit, error_unit
-      class(cm1_base), intent(in) :: self
-      integer, intent(in) :: loglevel
-      character(len=*), intent(in) :: funcname, message
+     use, intrinsic :: iso_fortran_env, only: output_unit, error_unit
+     implicit none
+     class(cm1_base), intent(in) :: self
+     integer, intent(in) :: loglevel
+     character(len=*), intent(in) :: funcname, message
+     
+     character(len=6) :: proto
+     
+     character(len=*),parameter :: fmt_error = "('[ingestcm1',3(':',A),']: Error: ',A)"
+     character(len=*),parameter :: fmt_warning = "('[ingestcm1',3(':',A),']: Warning: ',A)"
+     character(len=*),parameter :: fmt_message = "('[ingestcm1',3(':',A),']: ',A)"
+     
+     if (loglevel .lt. min_loglevel) return
+     
+     select case (self%dtype)
+        
+        case (GRADS)
+           proto = 'GRADS'
+        
+        case (GRADSMPI)
+           proto = 'GRMPI'
+        
+        case (HDF)
+           proto = "HDF5"
+        
+        case (GRADSSINGLE)
+           proto = "GRADS"
+        
+        case default
+           proto = 'X'
+        
+     end select
+     
+     select case (loglevel)
 
-      character(len=6) :: proto
-
-      1001 format('[ingestcm1',3(':',A),']: Error: ',A)
-      1002 format('[ingestcm1',3(':',A),']: Warning: ',A)
-      1004 format('[ingestcm1',3(':',A),']: ',A)
-
-      if (loglevel .lt. min_loglevel) return
-
-      select case (self%dtype)
-
-         case (GRADS)
-            proto = 'GRADS'
-
-         case (GRADSMPI)
-            proto = 'GRMPI'
-
-         case (HDF)
-            proto = "HDF5"
-         
-         case (GRADSSINGLE)
-            proto = "GRADS"
-
-         case default
-            proto = 'X'
-
-      end select
-
-      select case (loglevel)
-
-         case (LOG_ERROR)
-            write(error_unit,1001) trim(proto), self%grid, funcname, message
-
-         case (LOG_WARN)
-            write(error_unit,1002) trim(proto), self%grid, funcname, message
-
-         case (LOG_INFO)
-            write(output_unit,1004) trim(proto), self%grid, funcname, message
-
-         case (LOG_MSG)
-            write(output_unit,1004) trim(proto), self%grid, funcname, message
-
-         case (LOG_DEBUG)
-            write(output_unit,1004) trim(proto), self%grid, funcname, message
-
-      end select
-
-   end subroutine
-
-end module ingest_cm1_base
+        case (LOG_ERROR)
+           write(error_unit,fmt_error) trim(proto), self%grid, funcname, message
+        
+        case (LOG_WARN)
+           write(error_unit,fmt_warning) trim(proto), self%grid, funcname, message
+        
+        case (LOG_INFO)
+           write(output_unit,fmt_message) trim(proto), self%grid, funcname, message
+        
+        case (LOG_MSG)
+           write(output_unit,fmt_message) trim(proto), self%grid, funcname, message
+        
+        case (LOG_DEBUG)
+           write(output_unit,fmt_message) trim(proto), self%grid, funcname, message
+        
+     end select
+     
+   end subroutine cm1log
+   
+ end module ingest_cm1_base
